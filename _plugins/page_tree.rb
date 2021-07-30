fd223b4e413c1aa2dd6c563f3fe1338b27d18cd5
Johnny Baloney <itdev@gmx.com>
2021-07-30 15:38:08 +0100

    refactor

---
+++ _plugins/page_tree.rb
@@ -0,0 +1,144 @@
+require "set"
+
+class PageTree
+  attr_reader :depth
+  attr_reader :directory
+  attr_reader :pages
+  attr_reader :subdirectories
+  SEPARATOR = '/'
+
+  # TODO: fallback on url if title is absent
+  # TODO: support for empty folders
+  def initialize(depth, directory, parent_path, dir_to_title)
+    @depth = depth
+    @directory = directory
+    @parent_path = parent_path
+    @dir_to_title = dir_to_title
+    @pages = SortedSet.new
+    @subdirectories = SortedSet.new
+  end
+
+  def directory_title
+    @dir_to_title[directory_path]
+  end
+
+  def add_file_path(source_page)
+    dir_segments = source_page.path[1..].split(SEPARATOR)
+    add_file_path_list(source_page, dir_segments)
+  end
+
+  def add_file_path_list(source_page, dir_segments)
+    first = dir_segments[0]
+    other = dir_segments[1..]
+    if other.length == 0
+      @pages << WebPage.new(first, source_page.path, source_page.title, source_page.sort)
+      return
+    end
+    @subdirectories.each do |subdirectory|
+      if subdirectory.directory == first
+        subdirectory.add_file_path_list(source_page, other)
+        return
+      end
+    end
+    new_subdirectory = PageTree.new(@depth + 1, first, directory_path, @dir_to_title)
+    @subdirectories << new_subdirectory
+    new_subdirectory.add_file_path_list(source_page, other)
+  end
+
+  def directory_path
+    @parent_path + @directory + SEPARATOR
+  end
+
+  def <=>(o)
+    @directory <=> o.directory
+  end
+
+  def to_s
+    "PageTree{" +
+      "depth=#{@depth}" +
+      ", directory='#{@directory}'" +
+      ", parent_path='#{@parent_path}'" +
+      ", pages=[#{@pages.to_a.map{|p| p.to_s}.join(", ")}]" +
+      ", subdirectories=[#{@subdirectories.to_a.map{|s| s.to_s}.join(", ")}]}"
+  end
+end
+
+class SourcePage
+  attr_reader :path
+  attr_reader :title
+  attr_reader :sort
+
+  def initialize(path, title, sort)
+    @path = path
+    @title = title
+    @sort = sort
+  end
+
+  def to_s
+    "SourcePage{" +
+      "path=#{@path}" +
+      ", title='#{@title}'" +
+      ", sort=#{@sort}}"
+  end
+end
+
+class WebPage
+  attr_reader :file
+  attr_reader :path
+  attr_reader :title
+  attr_reader :sort
+
+  def initialize(file, path, title, sort = 0)
+    @file = file
+    @path = path
+    @title = title
+    @sort = sort
+  end
+
+  def <=>(o)
+    if sort == nil && o.sort != nil
+      1
+    elsif sort != nil && o.sort == nil
+      -1
+    elsif sort != nil && o.sort != nil
+      sort <=> o.sort
+    else
+      @file <=> o.file
+    end
+  end
+
+  def to_s
+    "WebPage{" +
+      "file=#{@file}" +
+      ", path='#{@path}'" +
+      ", title=#{@title}" +
+      ", sort=#{@sort}}"
+  end
+end
+
+def prepare_page_tree(context)
+  site_pages = lookup(context, 'site.html_pages')
+  # directory paths, e.g. ["/","/test_long/folder1/","/test_long/folder1/folder2/"...]
+  # directory titles, e.g. [null,"I’m folder1","I’m folder2",...]
+  # file urls, e.g. ["/about.html","/contactus.html","/test_long/folder1/file1.html"...]
+  # file titles, e.g. ["About","Contact Us","file1"...]
+  # file sort, e.g. [null,null,null,...,9,10,11]
+  site_pages_directories = site_pages.select { |page| page.dir == page.url }
+  site_pages_files = site_pages.select { |page| page.dir != page.url && (!page.url.include? "README.html") }
+  dir_to_title = {}
+  site_pages_directories.each do |page|
+    dir_to_title[page.dir] = page.data['title']
+  end
+  source_pages = []
+  site_pages_files.each do |page|
+    if page.url == '/404.html' || page.url == '/search.html'
+      next
+    end
+    source_pages << SourcePage.new(page.url, page.data['title'], page.data['sort'])
+  end
+  root = PageTree.new(0, "", "", dir_to_title)
+  source_pages.each do |source_page|
+    root.add_file_path(source_page)
+  end
+  root
+end
--- _plugins/render_sidebar.rb
+++ _plugins/render_sidebar.rb
@@ -6,45 +6,37 @@ module Jekyll
       @text = text
     end
 
-    # Access variable [name] from Liquid.
-    def lookup(context, name)
-      lookup = context
-      name.split(".").each { |value| lookup = lookup[value] }
-      lookup
-    end
-
     def top_level_page_link(page, root, current_page_path)
-      current = current_page_path == "/#{page.page}" ? "current" : ""
+      current = current_page_path == "/#{page.file}" ? "current" : ""
       link = "<li class=\"toc level-#{root.depth + 1} #{current}\" data-sort=\"#{page.sort}\" data-level=\"#{root.depth + 1}\">"
-      link += "<a class=\"d-flex flex-items-baseline \" href=\"/#{page.page}\">#{page.title}</a>"
+      link += "<a class=\"d-flex flex-items-baseline \" href=\"/#{page.file}\">#{page.title}</a>"
       link += "</li>"
       link
     end
 
-    def subdirectory_links(subdirectories, dir_to_title, current_page_path)
+    def subdirectory_links(subdirectories, current_page_path)
       links = ""
       subdirectories.each do |subdir|
-        abs_dir_path = subdir.absolute_dir_path
         if subdir.depth == 1
           # SUBDIRECTORIES - LEVEL 1
-          links += "<a class=\"caption d-block text-uppercase no-wrap px-2 py-0\" href=\"#{abs_dir_path}\">"
+          links += "<a class=\"caption d-block text-uppercase no-wrap px-2 py-0\" href=\"#{subdir.directory_path}\">"
         else
           # SUBDIRECTORIES - LEVEL 2+
           links += "<li class=\"toc level-#{subdir.depth - 1}\">"
-          links += "<a class=\"d-flex flex-items-baseline\" href=\"#{abs_dir_path}\">"
+          links += "<a class=\"d-flex flex-items-baseline\" href=\"#{subdir.directory_path}\">"
         end
-        links += dir_to_title[subdir.absolute_dir_path] != nil ? dir_to_title[subdir.absolute_dir_path] : subdir.directory
+        links += subdir.directory_title != nil ? subdir.directory_title : subdir.directory
         links += "</a>"
         links += "<ul>" # former '_toctree.liquid' start
         subdir.pages.each do |page|
-          current = current_page_path == "#{abs_dir_path}#{page.page}" ? "current" : ""
+          current = current_page_path == "#{subdir.directory_path}#{page.file}" ? "current" : ""
           links += "<li class=\"toc level-#{subdir.depth} #{current}\" data-sort=\"#{page.sort}\" data-level=\"#{subdir.depth}\">"
-          links += "<a class=\"d-flex flex-items-baseline \" href=\"#{abs_dir_path}#{page.page}\">"
+          links += "<a class=\"d-flex flex-items-baseline \" href=\"#{subdir.directory_path}#{page.file}\">"
           links += (page.sort != nil ? "#{page.sort}. " : "") + page.title
           links += "</a></li>"
         end
         if subdir.subdirectories.length > 0
-          links += subdirectory_links(subdir.subdirectories, dir_to_title, current_page_path)
+          links += subdirectory_links(subdir.subdirectories, current_page_path)
         end
         links += "</ul>" # former '_toctree.liquid' end
         if subdir.depth > 1
@@ -54,169 +46,27 @@ module Jekyll
       links
     end
 
-    def render_sidebar(root, dir_to_title, current_page_path)
+    def render_sidebar(root, current_page_path)
       # ROOT PAGES - LEVEL 0
       sidebar = "<ul>" # former 'toctree.liquid' start
       root.pages.each do |page|
         sidebar += top_level_page_link(page, root, current_page_path)
       end
       sidebar += "</ul>" # former 'toctree.liquid' end
-      sidebar += subdirectory_links(root.subdirectories, dir_to_title, current_page_path)
+      sidebar += subdirectory_links(root.subdirectories, current_page_path)
       sidebar
     end
 
     def render(context)
       # path of the currently processed page
       current_page_path = lookup(context, 'page.url')
-      site_pages = lookup(context, 'site.html_pages')
-      site_pages_directories = site_pages.select { |page| page.dir == page.url }
-      site_pages_files = site_pages.select { |page| page.dir != page.url && (!page.url.include? "README.html") }
-      # directory paths, e.g. ["/","/test_long/folder1/","/test_long/folder1/folder2/"...]
-      # directory titles, e.g. [null,"I’m folder1","I’m folder2",...]
-      _dir_to_title = {}
-      site_pages_directories.each do |page|
-        _dir_to_title[page.dir] = page.data['title']
-      end
-      # file urls, e.g. ["/about.html","/contactus.html","/test_long/folder1/file1.html"...]
-      # file titles, e.g. ["About","Contact Us","file1"...]
-      # file sort, e.g. [null,null,null,...,9,10,11]
-      _paths = []
-      site_pages_files.each do |page|
-        if page.url == '/404.html' || page.url == '/search.html'
-          next
-        end
-        _paths << Path.new(page.url, page.data['title'], page.data['sort'])
-      end
-      root = Node.new(0, "")
-      _paths.each do |path|
-        root.add_file_path(path)
-      end
-      render_sidebar(root, _dir_to_title, current_page_path)
-    end
-  end
-
-  require "set"
-
-  class Path
-    attr_reader :path
-    attr_reader :title
-    attr_reader :sort
-
-    def initialize(path, title, sort)
-      @path = path
-      @title = title
-      @sort = sort
-    end
-
-    def to_s
-      "Path{" +
-        "path=#{@path}" +
-        ", title='#{@title}'" +
-        ", sort=#{@sort}}"
-    end
-  end
-
-  class WebPage
-    attr_reader :page
-    attr_reader :path
-    attr_reader :title
-    attr_reader :sort
-
-    def initialize(page, path, title, sort)
-      @page = page
-      @path = path
-      @title = title
-      @sort = sort
-    end
-
-    def <=>(o)
-      if sort == nil && o.sort != nil
-        1
-      elsif sort != nil && o.sort == nil
-        -1
-      elsif sort != nil && o.sort != nil
-        sort <=> o.sort
-      else
-        @page <=> o.page
-      end
-    end
-
-    def to_s
-      "Page{" +
-        "page=#{@page}" +
-        ", path='#{@path}'" +
-        ", title=#{@title}" +
-        ", sort=#{@sort}}"
-    end
-  end
-
-  class Node
-    attr_reader :depth
-    attr_reader :directory
-    attr_reader :pages
-    attr_reader :subdirectories
-    attr_reader :parent
-    SEPARATOR = '/'
-
-    # TODO: remove @parent and 'absolute_dir_path' and store the directory @path instead
-    # TODO: fallback on url if title is absent
-    # TODO: support for empty folders
-    def initialize(depth, directory, parent = nil)
-      @parent = parent
-      @depth = depth
-      @directory = directory
-      @pages = SortedSet.new
-      @subdirectories = SortedSet.new
-    end
-
-    def add_file_path(path)
-      dir_segments = path.path[1..-1].split(SEPARATOR)
-      add_file_path_list(path, dir_segments)
-    end
-
-    def add_file_path_list(path, dir_segments)
-      first = dir_segments[0]
-      other = dir_segments[1..-1]
-      if other.length == 0
-        @pages << WebPage.new(first, path.path, path.title, path.sort)
-        return
-      end
-      @subdirectories.each do |subdirectory|
-        if subdirectory.directory == first
-          subdirectory.add_file_path_list(path, other)
-          return
-        end
-      end
-      new_subdirectory = Node.new(@depth + 1, first, self)
-      @subdirectories << new_subdirectory
-      new_subdirectory.add_file_path_list(path, other)
-    end
-
-    def absolute_dir_path
-      path = directory + SEPARATOR
-      node = self
-      while node.parent != nil do
-        node = node.parent
-        path = node.directory + SEPARATOR + path
-      end
-      path
-    end
-
-    def <=>(o)
-      @directory <=> o.directory
-    end
-
-    def to_s
-      "Node{" +
-        "depth=#{@depth}" +
-        ", directory='#{@directory}'" +
-        ", pages=#{@pages}" +
-        ", subdirectories=#{@subdirectories}}"
+      page_tree = prepare_page_tree(context)
+      render_sidebar(page_tree, current_page_path)
     end
   end
 
   def test
-    root = Node.new(0, "")
+    root = PageTree.new(0, "", "", {})
   end
 end
 
